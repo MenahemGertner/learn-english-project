95375d75f4a71af8259df33fc201846655f82aaa
Menahem_Gertner <meni1547@gmail.com>
2024-01-09 13:21:24 +0200

    add gitignore

--- ascii.py
+++ ascii.py
@@ -39,15 +39,3 @@ remote = '''           ⇓ After learning the meaning of the word ⇓
 |    and vice versa, read in Hebrew and translate to English!     |
  -----------------------------------------------------------------'''
 
-
-num = int(input("Enter a number: "))
-small = num
-large = num
-while num / 100 < 1 or num / 100 > 10:
-
-    num = int(input("Enter a number: "))
-    if num < small:
-        small = num
-    if num > large:
-        large = num
-print(f"The largest number is {large}, the smallest number is {small}")
